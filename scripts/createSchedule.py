f25ed1c4441bd102be619a85c336a737832d9380
Andy Famiglietti <afamiglietti@gmail.com>
2012-07-25 14:58:07 -0500

    skeleton of syllabus based on last year

---
+++ scripts/createSchedule.py
@@ -0,0 +1,22 @@
+# Creates Document with course meeting dates in Markdown format
+
+import time
+from datetime import timedelta
+from datetime import date
+f = open('syllabus_schedule.txt', 'w')
+
+theDay = date(2012, 8, 27)
+week = 1
+heading = "### Week " + str(week) + '\n\n'
+while theDay < date(2012, 12, 12):
+    f.write(heading) 
+    now = '#### ' + theDay.strftime("%A %B %d") + '\n\n\n'
+    f.write(now)
+    if theDay.weekday() == 2:
+        increment = timedelta(days = 5)
+        week = week + 1 			                
+        heading = "### Week " + str(week) + '\n\n'
+    else:        
+        increment = timedelta(days = 2)
+        heading = ""
+    theDay = theDay + increment
